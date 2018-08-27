fcff1ec97209fba108c20d75038e9f17cecd1edc
James <james@cleancoded.com>
2018-08-26 21:33:51 -0500

    Inital commit

---
+++ private-site.php
@@ -0,0 +1,83 @@
+<?php
+/*
+Plugin Name: Private Site
+Version: 0.2.0
+Description: Keep your WordPress website private.
+Author: CLEANCODED
+Author URI: https://cleancoded.com/
+Plugin URI: https://cleancoded.com/
+Text Domain: private-site
+*/
+
+class Private_Site {
+
+	private static $instance;
+
+	public static function get_instance() {
+
+		if ( ! isset( self::$instance ) ) {
+			self::$instance = new Private_Site;
+			self::$instance->setup_actions();
+		}
+
+		return self::$instance;
+	}
+
+	/**
+	 * WordPress API integrations
+	 */
+	private function setup_actions() {
+
+		add_action( 'template_redirect', array( $this, 'action_template_redirect' ) );	
+		add_filter( 'rest_authentication_errors', array( $this, 'filter_rest_authentication_errors' ) );
+	}
+
+	/**
+	 * Can this user view the WordPress website?
+	 * @return bool
+	 */
+	private function user_can_view() {
+
+		if ( is_user_logged_in() ) {
+
+			// Viewing the WordPress website requires users to be authenticated
+			$requires_membership = apply_filters( 'Private_Site_requires_membership', true );
+			if ( $requires_membership && ! is_user_member_of_blog() ) {
+				return false;
+			}
+
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * If a user isn't authenticated, redirect them to the login page
+	 */
+	public function action_template_redirect() {
+		global $pagenow;
+
+		if ( 'wp-login.php' !== $pagenow && ! $this->user_can_view() ) {
+			auth_redirect();
+		}
+	}
+
+	public function filter_rest_authentication_errors( $result ) {
+		if ( ! empty( $result ) ) {
+			return $result;
+		}
+		if ( ! self::user_can_view() ) {
+			return new WP_Error( 'restx_logged_out', 'Sorry, you must be logged in to view this website.', array( 'status' => 401 ) );
+		}
+		return $result;
+	}
+}
+
+/**
+ * Load plugin
+ */
+function Private_Site() {
+	return Private_Site::get_instance();
+}
+add_action( 'plugins_loaded', 'Private_Site' );
---
+++ readme.txt
@@ -0,0 +1,32 @@
+=== Private Site ===
+Contributors: cleancoded
+Tags: private site, private website, private WordPress, private blog
+Requires at least: 3.6
+Tested up to: 4.9
+Stable tag: 0.1
+
+Keep your WordPress website private and require all users to be logged in.
+
+== Description ==
+
+<strong>Private Site</strong> is a plugin that keeps your WordPress website private. When active, this plugin will redirect all unauthenticated users to the WordPress login screen.
+
+By default, the Private Site plugin requires users be added as a site member to view the website. You can broaden accessibility to any authenticated user by adding the following line to functions.php:
+
+`add_filter( 'Private_Site_requires_membership', '__return_false' );`
+
+== Installation ==
+
+1. Install Private Site by uploading the `private-site` directory to your `/wp-content/plugins/` directory. 
+2. Activate Private Site from the `Plugins` menu in WordPress.
+
+Activating the plugin will make your WordPress website private.
+
+== Frequently Asked Questions ==
+
+None
+
+== Changelog ==
+
+= 0.1 (Aug. 27, 2018) =
+* Initial release
